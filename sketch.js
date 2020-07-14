e83adc5f1e3ada253f829c5430fca596be5a06f5
dibyasundarpatra <64088971+dibyasundarpatra@users.noreply.github.com>
2020-07-14 19:26:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopterIMG=loadImage("sprites/helicopter.png")
+	packageIMG=loadImage("sprites/package.png")
 }
 
 function setup() {
